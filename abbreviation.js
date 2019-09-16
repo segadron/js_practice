bb0b474237347d65127118ee56822f296aefa8f0
segadron <43795594+segadron@users.noreply.github.com>
2019-09-16 19:54:36 +0300

    Delete abbreviation.js

--- abbreviation.js
+++
@@ -1,7 +0,0 @@
-const abbrevName = name => {
-  let arr = name.split(' ')
-  arr.forEach((item, i, arr) => arr[i] = item[0].toUpperCase())
-  return arr.join('.')
-}
-
-console.log(abbrevName('Sam Harris'))
